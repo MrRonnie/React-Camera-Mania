4aaba4bb55d526620d7fe17b1d8d4ba6e3922b13
MrRonnie <ronymaza55@gmail.com>
2022-04-05 01:50:36 +0600

    Navbar is made here all the components are connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Review from "./components/Review/Review";
+import Dashboard from "./components/Dashboard/Dashboard";
+import NotFound from "./components/NotFound/NotFound";
+import Blog from "./components/Blog/Blog";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-5xl">Welcome Boss!</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/review" element={<Review />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is Navbar</h1>
-    </div>
+    <nav>
+      <div className="name-container">
+        <h1 className="text-2xl">Camera Mania</h1>
+      </div>
+      <div className="link-container">
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/home"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/review"
+        >
+          Review
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/dashboard"
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/blog"
+        >
+          Blog
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : "link")}
+          to="/about"
+        >
+          About
+        </NavLink>
+      </div>
+    </nav>
   );
 };
 
