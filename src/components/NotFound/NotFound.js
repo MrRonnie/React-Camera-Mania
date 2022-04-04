b5b88756409464b14f8d99a17d5638df14d441c4
MrRonnie <ronymaza55@gmail.com>
2022-04-05 01:56:11 +0600

    404 Route Added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,9 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h1>This is not found</h1>
+      <h1 className="text-3xl text-bold mt-10 text-center text-red-600">
+        Ops!! 404 Page Not Found
+      </h1>
     </div>
   );
 };
