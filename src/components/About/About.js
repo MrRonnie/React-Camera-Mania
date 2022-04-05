dd6d18bb802539296c83e55e643e7b770d152e2f
MrRonnie <ronymaza55@gmail.com>
2022-04-05 21:55:47 +0600

    makeing some adjustment here

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is about us!</h1>
+    <div class="col-start-2 col-span-4 w-100  my-5 mx-80 rounded overflow-hidden shadow-lg text-justify p-5 mt-10">
+      <h1 className="text-center text-3xl font-serif font-bold">
+        What is an About Us Page?
+      </h1>
+      <p className=" text-center text-normal font-serif font-b mt-7">
+        An about us page is a specific page on your website where visitors can
+        go if they want to learn more about you or your company. They can be
+        called by many different names including about, about me, our story,
+        mission, etc. But, the purpose is always the same: To tell website
+        visitors who you are and why they should choose your business. And the
+        purpose is the same whether your'e a new blogger or a giant eCommerce
+        company.
+      </p>
     </div>
   );
 };
